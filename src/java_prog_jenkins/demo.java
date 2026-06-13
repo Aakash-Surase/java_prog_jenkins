0e00c8297b0bc886defe54c10662d95bdad51a76
Aakash-Surase <162134227+Aakash-Surase@users.noreply.github.com>
2026-06-13 15:12:41 +0530

    Initial Java project

---
+++ src/java_prog_jenkins/demo.java
@@ -0,0 +1,9 @@
+package java_prog_jenkins;
+
+public class demo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hii java prog");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module java_prog_jenkins {
+}
